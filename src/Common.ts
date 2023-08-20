3df293421a164eaeee22349815b1f2b5e70540ed
ciobi <ciobi07@users.sourceforge.net>
2023-08-20 16:27:44 +0300

    Added debugFmt(), to display objects and maps for debugging

--- src/Common.ts
+++ src/Common.ts
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import * as JSON5 from 'json5';
+
 // sort of ESLint bug - //!!! https://stackoverflow.com/questions/63961803/eslint-says-all-enums-in-typescript-app-are-already-declared-in-the-upper-scope
 // eslint-disable-next-line no-shadow
 export enum SortType {
@@ -13,3 +15,9 @@ export enum SortType {
 export type ReactGetter<T> = T | undefined;
 export type ReactSetter<T> = React.Dispatch<React.SetStateAction<T | undefined>>;
 export type ReactSetter2<T> = React.Dispatch<React.SetStateAction<T>>;  //ttt1: review the need for both ReactSetter and ReactSetter2
+
+export function debugFmt(x: any, multiLine: boolean = false): string {
+    const x1: any = (x instanceof Map) ? Object.fromEntries(x) : x;
+    return multiLine ? JSON5.stringify(x1, null, 2) : JSON.stringify(x1);
+}
+
