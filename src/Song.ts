af4eb5b3570b52dac1c386e9e2bfb67c62c14e4f
ciobi <ciobi07@users.sourceforge.net>
2023-09-01 09:40:31 +0300

    Moved function getAllChords() from UI to lib file

--- src/Song.ts
+++ src/Song.ts
@@ -1,3 +1,5 @@
+import {getRoot} from './ChordUtils';
+
 export type Stanza = {
     i?: string, // "id", or "index"; usually 1, 2, 3, ... , R, but also "^R2", "R^1", "^2", "^2a", "Intro", "^pre":
     // cat songs_db.js | grep ' i: "' | sort | uniq
@@ -93,3 +95,100 @@ function getFullTitleSomeRemoved(song: Song, removedLyricist?: string, removedPe
     const performers = removeElement(removedPerformer, song.p);
     return song.t + listsAsString(performers, lyricists);
 }
+
+
+/**
+ * The main purpose for this is to define alternative notes / chords, when a song can be sung in different ways
+ *
+ * Chord notes, like in "Noapte la mare" or "Cântec de inimă albastră": a "verse" that starts with "([", ends with "])",
+ * and has no other "[" or "]" (but might have "(" or ")"; after "([" there may be a comment, which ends with ": ";
+ * this is kept as is and what follows (or the whole thing if there is no comment) is passed through chord conversion;
+ *
+ * chord separators are defined in substituteChords;
+ * note that substituteChords does nothing if its parameter doesn't start with a chord
+ *
+ * @param verse
+ */
+function isChordNotes(verse: string) {
+    const n = verse.length;
+    //return n > 4 && verse[1] == "(" && verse[2] == "[" && verse.indexOf("]") == n - 2 && verse.indexOf(")") == n - 1; //!!! not right: "Cântec de inimă albastră" has inner ")"
+    return n > 4 && verse[0] === '(' && verse[1] === '[' && verse.indexOf(']') === n - 2 && verse[n - 1] === ')';
+}
+
+export function getAllChords(song: Song): string[] {
+
+    const chords = new Set<string>();
+    const deferredChords = new Set<string>(); //used to prevent some intro chords to be detected as "first chord"
+    for (let i = 0; i < song.b.length; ++i) { //!!! song["b"] is used rather than song.b to prevent Google Closure Compiler from renaming properties of an object that will be deserialized; see http://stackoverflow.com/questions/7823811/prevent-google-closure-compiler-from-renaming-settings-objects?rq=1 - "Unfortunately, doing data["hello"] all over the place is the recommended (and official) Closure way of preventing variable renaming."
+        const stanza = song.b[i];
+        const verses = stanza.v;
+        if (!verses) {
+            continue;
+        }
+        for (let j = 0; j < verses.length; ++j) {
+            const verse = verses[j];
+            if (isChordNotes(verse)) { // it's an alternative range or some other note, which we just ignore here
+                continue;
+            }
+            let k = verse.indexOf('[');
+            /*if (verse.indexOf("restul e numa") != -1) {
+                debugger;
+            }//*/
+            //var deferred = k == 0 && verse.indexOf("]") == verse.length - 1; //ttt3 review; probably quite confusing when intro is ignored; so as of 2015-12-21 nothing is actually deferred, by setting deferred=false
+            const deferred = false;
+            for (;;) {
+                if (k === -1) {
+                    break;
+                }
+                const h = verse.indexOf(']', k);
+                if (h === -1) {
+                    break;
+                }
+                let a = verse.substring(k + 1, h).split(' '); // regular list of chords
+                if (a.length === 1) {
+                    let a2 = verse.substring(k + 1, h);
+                    if (a2.startsWith(';')) {
+                        a2 = a2.substring(1);
+                    }
+                    a = a2.split(';'); // chords that change in the same vowel
+                }
+                for (let c = 0; c < a.length; ++c) {
+                    let chord = a[c].trim();
+                    if (chord !== 'N' && chord !== '/') {
+                        if (chord.startsWith('1:') || chord.startsWith('2:')) {
+                            chord = chord.substring(2);
+                        }
+                        if (getRoot(chord)) {
+                            if (deferred) {
+                                deferredChords.add(chord);
+                            } else {
+                                chords.add(chord);
+                            }
+                        } else {
+                            //console.log("got non-chord passed as chord: '" + chord + "' in verse '" + verse + "'");
+                            throw Error(`got non-chord passed as chord: '${chord}' in verse '${verse}'`);
+                        }
+                    }
+                }
+                k = verse.indexOf('[', h);
+            }
+        }
+    }
+
+    //const res: string[] = [];
+
+    /*for (const chord in deferredChords) {  //ttt0: make sure the sets implement the original functionality
+        if (deferredChords.hasOwnProperty(chord)) {
+            chords[chord] = 1;
+        }
+    }
+    for (const chord in chords) {
+        if (chords.hasOwnProperty(chord)) {
+            res.push(chord);
+        }
+    }*/
+    //return [...chords, ...deferredChords]; // requires ES2015, let's not force it
+    deferredChords.forEach((s) => chords.add(s));
+    return Array.from(chords);
+}
+
--- src/widgets/Song.tsx
+++ src/widgets/Song.tsx
@@ -3,109 +3,13 @@ import React from 'react';
 
 import '../legacy.css';
 import {SongRenderConfig} from '../SongRenderConfig';
-import {Song} from '../Song';
+import {getAllChords, Song} from '../Song';
 import {ReactSetter2} from '../Common';
-import {getRoot} from '../ChordUtils';
 import {ChordsAreaWidget} from './chords/ChordsArea';
 
 
 //ttt0 check when same person is lyricist / performer / composer ... and keep one instance of each name, separate by comma
 
-
-/**
- * The main purpose for this is to define alternative notes / chords, when a song can be sung in different ways
- *
- * Chord notes, like in "Noapte la mare" or "Cântec de inimă albastră": a "verse" that starts with "([", ends with "])",
- * and has no other "[" or "]" (but might have "(" or ")"; after "([" there may be a comment, which ends with ": ";
- * this is kept as is and what follows (or the whole thing if there is no comment) is passed through chord conversion;
- *
- * chord separators are defined in substituteChords;
- * note that substituteChords does nothing if its parameter doesn't start with a chord
- *
- * @param verse
- */
-function isChordNotes(verse: string) {
-    const n = verse.length;
-    //return n > 4 && verse[1] == "(" && verse[2] == "[" && verse.indexOf("]") == n - 2 && verse.indexOf(")") == n - 1; //!!! not right: "Cântec de inimă albastră" has inner ")"
-    return n > 4 && verse[0] === '(' && verse[1] === '[' && verse.indexOf(']') === n - 2 && verse[n - 1] === ')';
-}
-
-function getAllChords(song: Song): string[] {
-
-    const chords = new Set<string>();
-    const deferredChords = new Set<string>(); //used to prevent some intro chords to be detected as "first chord"
-    for (let i = 0; i < song.b.length; ++i) { //!!! song["b"] is used rather than song.b to prevent Google Closure Compiler from renaming properties of an object that will be deserialized; see http://stackoverflow.com/questions/7823811/prevent-google-closure-compiler-from-renaming-settings-objects?rq=1 - "Unfortunately, doing data["hello"] all over the place is the recommended (and official) Closure way of preventing variable renaming."
-        const stanza = song.b[i];
-        const verses = stanza.v;
-        if (!verses) {
-            continue;
-        }
-        for (let j = 0; j < verses.length; ++j) {
-            const verse = verses[j];
-            if (isChordNotes(verse)) { // it's an alternative range or some other note, which we just ignore here
-                continue;
-            }
-            let k = verse.indexOf('[');
-            /*if (verse.indexOf("restul e numa") != -1) {
-                debugger;
-            }//*/
-            //var deferred = k == 0 && verse.indexOf("]") == verse.length - 1; //ttt3 review; probably quite confusing when intro is ignored; so as of 2015-12-21 nothing is actually deferred, by setting deferred=false
-            const deferred = false;
-            for (;;) {
-                if (k === -1) {
-                    break;
-                }
-                const h = verse.indexOf(']', k);
-                if (h === -1) {
-                    break;
-                }
-                let a = verse.substring(k + 1, h).split(' '); // regular list of chords
-                if (a.length === 1) {
-                    let a2 = verse.substring(k + 1, h);
-                    if (a2.startsWith(';')) {
-                        a2 = a2.substring(1);
-                    }
-                    a = a2.split(';'); // chords that change in the same vowel
-                }
-                for (let c = 0; c < a.length; ++c) {
-                    let chord = a[c].trim();
-                    if (chord !== 'N' && chord !== '/') {
-                        if (chord.startsWith('1:') || chord.startsWith('2:')) {
-                            chord = chord.substring(2);
-                        }
-                        if (getRoot(chord)) {
-                            if (deferred) {
-                                deferredChords.add(chord);
-                            } else {
-                                chords.add(chord);
-                            }
-                        } else {
-                            //console.log("got non-chord passed as chord: '" + chord + "' in verse '" + verse + "'");
-                            throw Error(`got non-chord passed as chord: '${chord}' in verse '${verse}'`);
-                        }
-                    }
-                }
-                k = verse.indexOf('[', h);
-            }
-        }
-    }
-
-    //const res: string[] = [];
-
-    /*for (const chord in deferredChords) {  //ttt0: make sure the sets implement the original functionality
-        if (deferredChords.hasOwnProperty(chord)) {
-            chords[chord] = 1;
-        }
-    }
-    for (const chord in chords) {
-        if (chords.hasOwnProperty(chord)) {
-            res.push(chord);
-        }
-    }*/
-    //return [...chords, ...deferredChords]; // requires ES2015, let's not force it
-    deferredChords.forEach((s) => chords.add(s));
-    return Array.from(chords);
-}
 
 
 export const SongWidget = ({
