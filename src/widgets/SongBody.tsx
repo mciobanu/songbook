d0d90dfea6777aa5421a6efa6c392d9121cd9ff9
ciobi <ciobi07@users.sourceforge.net>
2023-09-03 12:53:56 +0300

    Added measurements when rendering the song, so additional "-" signs can be generated if needed.

--- src/widgets/SongBody.tsx
+++ src/widgets/SongBody.tsx
@@ -25,12 +25,16 @@ enum ChordRendering {
 }
 
 
+type WidthCalculator = (s: string) => number;
+
 const ChordTableWidget = ({
     text,
     last,
+    widthCalculator,
 } : {
     text: string,
     last: boolean,
+    widthCalculator: WidthCalculator,
 }) => {
 
     const rowsInfo = React.useMemo(() => {
@@ -100,41 +104,72 @@ const ChordTableWidget = ({
     }, [chordNotes, rowsInfo.row1]);
 
     const textRow = React.useMemo(() => {
-        /*const n = text.length;
-        let k = 0;
-        let chordsFound = false;
-        let tr1;
-        let tr2;
-        let td;
-        let g = 0;
-        let h;*/
         if (chordNotes) {
             return null;
         }
-        /*  //ttt9: make measurements work
-                if (i < tr2Elems.length - 1 && t2 && !t2.endsWith("-") && !t2.endsWith(" ")) {
-                    //tds.push([td1, td2]);
-                    var t1 = tr1Elems[i];
-                    uiMeasure.textContent = t1 + "|"; // innerHTML for IE < 9
-                    var w1 = uiMeasure.scrollWidth;
-                    uiMeasure.textContent = t2 + "|"; // innerHTML for IE < 9
+
+        /*return <tr>
+            {rowsInfo.row2.map((s, index) => {
+                let t2 = s;
+                if (index < rowsInfo.row2.length - 1 && !s.endsWith('-') && !s.endsWith(' ')) { //ttt0: Make sure this works with zoom
+                    // Check if we should add a dash, for cases when the chord above takes more space than the text
+                    // below
+                    const t1 = rowsInfo.row1[index];
+                    //const output = document.createElement('span')
+                    //const uiMeasure1 = document.getElementById('uiMeasure');
+                    const uiMeasure1 = uiMeasure;
+
+                    uiMeasure1.textContent = `${t1}|`; // innerHTML for IE < 9
+                    const w1 = uiMeasure.scrollWidth;
+                    uiMeasure1.textContent = `${t2}|`; // innerHTML for IE < 9
                     //ttt0 Țigăncușa - verse 2 with small fonts and some chords there's no "-" between "ți" and "gani", but there is space
-                    var w2 = uiMeasure.scrollWidth;
-                    var w3 = uiMeasure.style.width;
+                    const w2 = uiMeasure.scrollWidth;
+                    //const w3 = uiMeasure.style.width;
                     //console.log(uiMeasure.scrollWidth);
                     if (w1 > w2) {
-                        t2 += "-"; //!!! this adds a "-" when the chord name is longer than the text part ("Și restul e numai Chopin și tăce-e   re."  - initially there was a space after "tăce-e")
+                        t2 += '-'; //!!! This adds a "-" when the chord name is longer than the text part
+                        // Without it: "Și restul e numai Chopin și tăce-e   re."
+                        // With it:    "Și restul e numai Chopin și tăce-e-  re."
                         //ttt2 perhaps allow to define a "non-printable dash", that is shown only inside chords, to cover cases where this default doesn't quite work
                     }
+                    //console.log(t1);
+                    //console.log(`uiMeasure1:${uiMeasure1}`);
                 }
 
-        */
+                return <td className='chordCellText' key={index}>{t2}</td>;
+            })}
+        </tr>;*/
+
         return <tr>
             {rowsInfo.row2.map((s, index) => {
-                return <td className='chordCellText' key={index}>{s}</td>;
+                let t2 = s;
+                if (index < rowsInfo.row2.length - 1 && !s.endsWith('-') && !s.endsWith(' ')) { //ttt0: Make sure this works with zoom
+                    // Check if we should add a dash, for cases when the chord above takes more space than the text
+                    // below
+                    const t1 = rowsInfo.row1[index];
+                    //const output = document.createElement('span')
+                    //const uiMeasure1 = document.getElementById('uiMeasure');
+
+                    const w1 = widthCalculator(`${t1}|`);
+                    //ttt0 Țigăncușa - verse 2 with small fonts and some chords there's no "-" between "ți" and "gani", but there is space
+                    const w2 = widthCalculator(`${t2}|`);
+                    //const w3 = uiMeasure.style.width;
+                    //console.log(uiMeasure.scrollWidth);
+                    if (w1 > w2) {
+                        t2 += '-'; //!!! This adds a "-" when the chord name is longer than the text part
+                        // Without it: "Și restul e numai Chopin și tăce-e   re."
+                        // With it:    "Și restul e numai Chopin și tăce-e-  re."
+                        //ttt2 perhaps allow to define a "non-printable dash", that is shown only inside chords, to cover cases where this default doesn't quite work
+                    }
+                    //console.log(t1);
+                    //console.log(`uiMeasure1:${uiMeasure1}`);
+                }
+
+                return <td className='chordCellText' key={index}>{t2}</td>;
             })}
         </tr>;
-    }, [chordNotes, rowsInfo.row2]);
+
+    }, [chordNotes, rowsInfo.row1, rowsInfo.row2, widthCalculator]);
 
 
     if (!rowsInfo.chordsFound) {
@@ -164,6 +199,7 @@ const VerseWidget = ({
     chordRendering,
     capo,
     rangeShift,
+    widthCalculator,
 } : {
     verse: string,
     firstVerse: boolean,
@@ -173,6 +209,7 @@ const VerseWidget = ({
     chordRendering: ChordRendering,
     capo: number,
     rangeShift: number,
+    widthCalculator: WidthCalculator,
 }) => {
     // noinspection UnnecessaryLocalVariableJS
     const res = React.useMemo(() => {
@@ -192,7 +229,7 @@ const VerseWidget = ({
         if (chordRendering === ChordRendering.ABOVE) {
             const text = replaceChordSequence(changeStanzaChords(v, capo, rangeShift, false));
             if (text) {
-                return <ChordTableWidget text={text} last={lastVerse}></ChordTableWidget>;
+                return <ChordTableWidget text={text} last={lastVerse} widthCalculator={widthCalculator}/>;
             }
             return null;
         }
@@ -206,7 +243,7 @@ const VerseWidget = ({
         }
 
         return <p className={lastVerse ? 'songLastVerse' : 'songNormalVerse'}>{text}</p>;
-    }, [capo, chordRendering, firstVerse, lastVerse, rangeShift, repeat, stanzaId, verse]);
+    }, [capo, chordRendering, firstVerse, lastVerse, rangeShift, repeat, stanzaId, widthCalculator, verse]);
 
     return res;
 };
@@ -217,11 +254,13 @@ const StanzaWidget = ({
     chordRendering,
     capo,
     rangeShift,
+    widthCalculator,
 } : {
     stanza: Stanza,
     chordRendering: ChordRendering,
     capo: number,
     rangeShift: number,
+    widthCalculator: WidthCalculator,
 }) => {
     //!!! Handling of empty verses: Nothing is shown for such a verse, but the verse above is marked as "last", which
     // adds some space.
@@ -232,10 +271,10 @@ const StanzaWidget = ({
         return v.map((verse, index) => {
             return <VerseWidget verse={verse} firstVerse={index === 0} stanzaId={index === 0 ? stanza.i : undefined}
                 repeat={stanza.r} chordRendering={chordRendering} capo={capo} rangeShift={rangeShift}
-                lastVerse={index === v.length - 1 || !v[index + 1]} key={index}
+                lastVerse={index === v.length - 1 || !v[index + 1]} key={index} widthCalculator={widthCalculator}
             />;
         });
-    }, [capo, chordRendering, rangeShift, stanza.i, stanza.r, stanza.v]);
+    }, [capo, chordRendering, rangeShift, stanza.i, stanza.r, stanza.v, widthCalculator]);
 
     return <>
         {verses}
@@ -266,13 +305,61 @@ export const SongBodyWidget = ({
         return true; // This is supposed to make sure that cloneEmptyStanzas() is called once before doing anything about stanzas
     }, [song]);
 
+    // Create an invisible DOM element directly, to be used for measurements
+    let uiMeasure: HTMLSpanElement;
+    const existingUiMeasure = document.getElementById('uiMeasure');
+    if (existingUiMeasure) {
+        uiMeasure = existingUiMeasure;
+    } else {
+        uiMeasure = document.createElement('span');
+        uiMeasure.id = 'uiMeasure';
+        uiMeasure.className = 'measureSpan';
+        //existingUiMeasure = document.getElementById('uiMeasure');
+        //const rootNode = document.getRootNode();
+        const rootNode = document.getElementById('root');
+        if (rootNode) {
+            rootNode.appendChild(uiMeasure);
+            //existingUiMeasure = document.getElementById('uiMeasure');
+        }
+    }
+
+    const widthCalculator = React.useCallback((s: string): number => {
+        uiMeasure.textContent = s;   // innerHTML for IE < 9
+        return uiMeasure.scrollWidth;
+    }, [uiMeasure]);  //ttt9: see if uiMeasure should be Memo, to prevent recomputing at every render
+
+    console.log(widthCalculator('abc'));
+
+    //ttt2: See if this can replace uiMeasure. The idea is to use a canvas to measure things, but one issue is getting
+    // the attributes from the "measureSpan" class, namely the font, including zoom changes. The other issue is that
+    // the computations seem to work only for elements that are actually inserted in the DOM, not merely created.
+    // If that is really the case, there's little point in this alternative
+    /*const text = 'text';
+    const span = document.createElement('span');
+    span.className = 'measureSpan';
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    if (context) {
+        const st = getComputedStyle(span); // this has no properties
+        const rootNode = document.getElementById('root');
+        if (rootNode) {
+            console.log(`font:${st.font}`); // nothing
+            rootNode.appendChild(span); //!!! this seems needed to get any useful style info
+            console.log(`font:${st.font}`); // 14.4px Verdana, Geneva, sans-serif
+        }
+        context.font = st.font;  //ttt2: Set actual font
+        const {width} = context.measureText(text);
+        console.log(`measureText: ${width}`);
+    }*/
+
     // noinspection UnnecessaryLocalVariableJS
     const stanzas = React.useMemo(() => {
-        return songReady ? <> {song.b.map((stanza, index) => {
-            return <StanzaWidget stanza={stanza} chordRendering={computeChordRendering(songRenderConfig)}
-                capo={capo} rangeShift={rangeShift} key={index}/>;
-        })} </> : null;
-    }, [capo, rangeShift, song.b, songReady, songRenderConfig]);
+        return songReady ? <>
+            {song.b.map((stanza, index) => {
+                return <StanzaWidget stanza={stanza} chordRendering={computeChordRendering(songRenderConfig)}
+                    capo={capo} rangeShift={rangeShift} key={index} widthCalculator={widthCalculator}/>;
+            })} </> : null;
+    }, [capo, rangeShift, song.b, songReady, songRenderConfig, widthCalculator]);
 
     return stanzas;
 };
